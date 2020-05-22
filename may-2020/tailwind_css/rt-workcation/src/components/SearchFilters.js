4c6a7701dd37f36a5abe2c2f45c0f43ff6608f96
Kevin Jose <kevin.jose@razorpay.com>
2020-05-22 10:51:47 +0530

    [22/5/2020][TAILWIND_WORKCATION] added tabs one with a form content

--- may-2020/tailwind_css/rt-workcation/src/components/SearchFilters.js
+++ may-2020/tailwind_css/rt-workcation/src/components/SearchFilters.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useToogle from '../hooks/useToggle';
 import Checkbox from './Checkbox';
 import Radio from './Radio';
 
 const SearchFilters = () => {
   const [isOpen, setOpen] = useToogle(false);
+  const [openTab, setOpenTab] = useState(1);
 
   return (
     <section className='bg-gray-800 xl:w-1/4 xl:h-full'>
@@ -39,142 +40,193 @@ const SearchFilters = () => {
           >
             <path d='M3 6a1 1 0 0 1 1-1h16a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1zm3 6a1 1 0 0 1 1-1h10a1 1 0 1 1 0 2H7a1 1 0 0 1-1-1zm4 5a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-4z' />
           </svg>
-          <span className='ml-1 text-white font-medium'>Filters</span>
+          <span className='ml-1 text-white font-medium'>Edit</span>
         </button>
       </div>
-      <form
-        className={`xl:block xl:h-full xl:flex xl:flex-col xl:justify-between ${
+      {/* Tabbed section start */}
+
+      <div
+        className={`xl:block w-full flex flex-col xl:h-full ${
           isOpen ? 'block' : 'hidden'
-        }`}
+        } `}
       >
-        {/* Have a container that is scrollable along y axis if filters increase  also to justify content */}
-        <div className='lg:flex xl:block xl:overflow-y-auto'>
-          {/* Container for form elements providing padding border etc. also group a few filters*/}
-          <div className='px-4 py-4 border-t border-gray-900 lg:w-1/3 border xl:w-full'>
-            <div className='-mx-2 flex flex-wrap'>
-              <label className='block w-1/2 px-2 sm:w-1/4 lg:w-1/2'>
-                <span className='text-sm font-semibold text-gray-500'>
-                  Title
-                </span>
-                <select className='mt-1 block w-full focus:text-white text-gray-900 shadow focus:bg-gray-600 h-8 outline-none'>
-                  <option></option>
-                  <option>4</option>
-                </select>
-              </label>
-              <label className='block w-1/2 px-2 sm:w-1/4 lg:w-1/2'>
-                <span className='text-sm font-semibold text-gray-500'>
-                  Title
-                </span>
-                <select className='mt-1 block w-full focus:text-white text-gray-900 shadow focus:bg-gray-600 h-8 outline-none'>
-                  <option></option>
-                  <option>2</option>
-                  <option>3</option>
-                </select>
-              </label>
-              <label className='mt-4 lg:mt-4 sm:mt-0 block w-full px-2 sm:w-1/2 lg:w-full'>
-                <span className='text-sm font-semibold text-gray-500'>
-                  Title
-                </span>
-                <select className='mt-1 block w-full focus:text-white text-gray-900 shadow focus:bg-gray-600 h-8 outline-none'>
-                  <option></option>
-                  <option>Up to $2,000/wk</option>
-                </select>
-              </label>
+        <ul
+          className='flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row px-3'
+          role='tablist'
+        >
+          <li className='mr-2 flex-auto text-center'>
+            <a
+              className={
+                'text-xs font-bold uppercase px-5 py-3 block leading-normal ' +
+                (openTab === 1
+                  ? 'text-white border-b border-white'
+                  : 'text-gray-600')
+              }
+              onClick={e => {
+                e.preventDefault();
+                setOpenTab(1);
+              }}
+              data-toggle='tab'
+              href='#link1'
+              role='tablist'
+            >
+              Content
+            </a>
+          </li>
+          <li className='mr-2 flex-auto text-center'>
+            <a
+              className={
+                'text-xs font-bold uppercase px-5 py-3 block leading-normal ' +
+                (openTab === 2
+                  ? 'text-white border-b border-white'
+                  : 'text-gray-600')
+              }
+              onClick={e => {
+                e.preventDefault();
+                setOpenTab(2);
+              }}
+              data-toggle='tab'
+              href='#link2'
+              role='tablist'
+            >
+              Settings
+            </a>
+          </li>
+        </ul>
+        {/* Tab 1 content */}
+        <form
+          className={` xl:flex-col xl:justify-between ${
+            openTab === 1 ? 'xl:flex block' : 'hidden'
+          } xl:pb-16 h-64 overflow-y-auto xl:h-full`}
+        >
+          <div className='lg:flex xl:block'>
+            <div className='px-4 py-4 border-t border-gray-900 lg:w-1/3 border xl:w-full'>
+              <div className='-mx-2 flex flex-wrap'>
+                <label className='block w-1/2 px-2 sm:w-1/4 lg:w-1/2'>
+                  <span className='text-sm font-semibold text-gray-500'>
+                    Title
+                  </span>
+                  <select className='mt-1 block w-full focus:text-white text-gray-900 shadow focus:bg-gray-600 h-8 outline-none'>
+                    <option></option>
+                    <option>4</option>
+                  </select>
+                </label>
+                <label className='block w-1/2 px-2 sm:w-1/4 lg:w-1/2'>
+                  <span className='text-sm font-semibold text-gray-500'>
+                    Title
+                  </span>
+                  <select className='mt-1 block w-full focus:text-white text-gray-900 shadow focus:bg-gray-600 h-8 outline-none'>
+                    <option></option>
+                    <option>2</option>
+                    <option>3</option>
+                  </select>
+                </label>
+                <label className='mt-4 lg:mt-4 sm:mt-0 block w-full px-2 sm:w-1/2 lg:w-full'>
+                  <span className='text-sm font-semibold text-gray-500'>
+                    Title
+                  </span>
+                  <select className='mt-1 block w-full focus:text-white text-gray-900 shadow focus:bg-gray-600 h-8 outline-none'>
+                    <option></option>
+                    <option>Up to $2,000/wk</option>
+                  </select>
+                </label>
+              </div>
+            </div>
+            <div className='px-4 py-4 border-t border-gray-900 lg:w-1/3 border xl:w-full'>
+              <span className='block text-sm font-semibold text-gray-500'>
+                Property name
+              </span>
+              <div className='sm:flex sm:-mx-2 lg:block lg:mx-0'>
+                <Radio
+                  content={'A'}
+                  value={'a'}
+                  name={'propertyType'}
+                  color='white'
+                  background='gray'
+                />
+                <Radio
+                  content={'B'}
+                  value={'b'}
+                  name={'propertyType'}
+                  color='white'
+                  background='gray'
+                />
+                <Radio
+                  content={'C'}
+                  value={'c'}
+                  name={'propertyType'}
+                  color='white'
+                  background='gray'
+                />
+                <Radio
+                  content={'D'}
+                  value={'d'}
+                  name={'propertyType'}
+                  color='white'
+                  background='gray'
+                />
+              </div>
+            </div>
+            <div className='px-4 py-4 border-t border-gray-900 lg:w-1/3 border xl:w-full'>
+              <span className='block text-sm font-semibold text-gray-500'>
+                Property name
+              </span>
+              <div className='sm:flex sm:-mx-2 sm:flex-wrap'>
+                <Checkbox
+                  name={'a'}
+                  content={'A'}
+                  color='white'
+                  background='gray'
+                />
+                <Checkbox
+                  name={'b'}
+                  content={'B'}
+                  color='white'
+                  background='gray'
+                />
+                <Checkbox
+                  name={'c'}
+                  content={'C'}
+                  color='white'
+                  background='gray'
+                />
+                <Checkbox
+                  name={'d'}
+                  content={'D'}
+                  color='white'
+                  background='gray'
+                />
+                <Checkbox
+                  name={'e'}
+                  content={'E'}
+                  color='white'
+                  background='gray'
+                />
+                <Checkbox
+                  name={'f'}
+                  content={'F'}
+                  color='white'
+                  background='gray'
+                />
+                <Checkbox
+                  name={'g'}
+                  content={'G'}
+                  color='white'
+                  background='gray'
+                />
+              </div>
             </div>
           </div>
-          <div className='px-4 py-4 border-t border-gray-900 lg:w-1/3 border xl:w-full'>
-            <span className='block text-sm font-semibold text-gray-500'>
-              Property name
-            </span>
-            <div className='sm:flex sm:-mx-2 lg:block lg:mx-0'>
-              <Radio
-                content={'A'}
-                value={'a'}
-                name={'propertyType'}
-                color='white'
-                background='gray'
-              />
-              <Radio
-                content={'B'}
-                value={'b'}
-                name={'propertyType'}
-                color='white'
-                background='gray'
-              />
-              <Radio
-                content={'C'}
-                value={'c'}
-                name={'propertyType'}
-                color='white'
-                background='gray'
-              />
-              <Radio
-                content={'D'}
-                value={'d'}
-                name={'propertyType'}
-                color='white'
-                background='gray'
-              />
-            </div>
+          <div className='bg-gray-900 px-4 py-4 sm:text-right'>
+            <button
+              className={`block w-full sm:w-auto sm:inline-block bg-blue-800 hover:bg-blue-700 font-semibold text-white px-4 py-2 rounded-lg xl:block xl:w-full`}
+            >
+              Filter out
+            </button>
           </div>
-          <div className='px-4 py-4 border-t border-gray-900 lg:w-1/3 border xl:w-full'>
-            <span className='block text-sm font-semibold text-gray-500'>
-              Property name
-            </span>
-            <div className='sm:flex sm:-mx-2 sm:flex-wrap'>
-              <Checkbox
-                name={'a'}
-                content={'A'}
-                color='white'
-                background='gray'
-              />
-              <Checkbox
-                name={'b'}
-                content={'B'}
-                color='white'
-                background='gray'
-              />
-              <Checkbox
-                name={'c'}
-                content={'C'}
-                color='white'
-                background='gray'
-              />
-              <Checkbox
-                name={'d'}
-                content={'D'}
-                color='white'
-                background='gray'
-              />
-              <Checkbox
-                name={'e'}
-                content={'E'}
-                color='white'
-                background='gray'
-              />
-              <Checkbox
-                name={'f'}
-                content={'F'}
-                color='white'
-                background='gray'
-              />
-              <Checkbox
-                name={'g'}
-                content={'G'}
-                color='white'
-                background='gray'
-              />
-            </div>
-          </div>
-        </div>
-        <div className='bg-gray-900 px-4 py-4 sm:text-right'>
-          <button
-            className={`block w-full sm:w-auto sm:inline-block bg-blue-800 hover:bg-blue-700 font-semibold text-white px-4 py-2 rounded-lg xl:block xl:w-full`}
-          >
-            Filter out
-          </button>
-        </div>
-      </form>
+        </form>
+      </div>
+      {/* Tabbed section end */}
     </section>
   );
 };
